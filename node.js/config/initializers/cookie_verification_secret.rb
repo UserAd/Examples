14d525fa0019b5b83378efb2c5dc016e6f675dcf
UserAd <userad@gmail.com>
2010-09-08 20:05:55 +0400

    Setting up a new rails app. Copy config/database.yml.sample to config/database.yml and customize.

---
+++ node.js/config/initializers/cookie_verification_secret.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random, 
+# no regular words or you'll be exposed to dictionary attacks.
+ActionController::Base.cookie_verifier_secret = '84cdca3ebacc00f16029890b2942dc29b0c78d063ac789f7d2f5869787d9564f6f2de2e1fcb8a7b56b747fac9b7f2ac136d309b606671e466a4361ee6976907f';
